ef3a565f55d95244411f4ee0fceb4586709044a5
rayanserrao <rayanserrao25@gmail.com>
2021-02-04 01:54:18 +0530

    added login and template

--- app_users/views.py
+++ app_users/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from app_users.forms import UserForm,UserProfileForm
+from django.contrib.auth import authenticate,login,logout
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello word")
+    return render(request,'home.html')
 
 def registeruser(request):
     registerd = False
@@ -33,10 +36,33 @@ def registeruser(request):
     return render(request,'register.html',{'registred':registerd,'user_form':user_form,'profile_form':profile_form})
 
 
-# def loginuser(request):
-#     if not request.user.is_authenticated:
-#         if request.method == 'POST':
-#             form = UserForm(request=request,data=request.POST)
-#             if form.is_valid():
-#                 username = form.cleaned_data['username']
-#                 password = form.cleaned_data['password']
+def loginuser(request):
+    if not request.user.is_authenticated:
+
+
+
+   
+        if request.method == 'POST':
+
+                
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('/index/')
+                else:
+                    return HttpResponse("Account deactivated")
+            else:
+                return HttpResponse("Please use valid id and password")
+    else:
+        return redirect("/index/")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('/index/')
